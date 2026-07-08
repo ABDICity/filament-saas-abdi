431a305d074df482f2b84fa21bb379929274a4c4
Benn BIGGroup Al Islamicity <masjid.islamicity@gmail.com>
2026-07-08 16:37:22 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,17 @@
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+// Rute Bypass Publik untuk Login API di Web Group
+Route::post('/api/external-login', function (\Illuminate\Http\Request $request) {
+    $user = \App\Models\User::where('email', $request->email)->first();
+
+    if (! $user || !\Hash::check($request->password, $user->password)) {
+        return response()->json(['message' => 'Kredensial salah.'], 401);
+    }
+
+    return response()->json([
+        'status' => 'success',
+        'token' => $user->createToken('ABDI-Cloud-Token')->plainTextToken
+    ]);
+});

97139a78c9b3c31175fc8f973dd824761032a0ba
Benn BIGGroup Al Islamicity <masjid.islamicity@gmail.com>
2026-07-08 14:07:34 +0700

    api update

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/generate-token-abdi', function () {
+    $user = \App\Models\User::where('email', 'masjid1.islamicity@gmail.com')->first();
+    if ($user) {
+        return response()->json(['token' => $user->createToken('Postman-Token')->plainTextToken]);
+    }
+    return response()->json(['error' => 'User tidak ditemukan'], 404);
+});
